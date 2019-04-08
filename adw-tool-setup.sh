4a428e7154de02e0b129352c27536c433a1f6e7b
jhan <35212672+orajhan@users.noreply.github.com>
2019-04-08 17:13:23 +0800

    Update adw-tool-setup.sh

--- adw-tool-setup.sh
+++ adw-tool-setup.sh
@@ -15,9 +15,9 @@ echo $docker_password | sudo docker login lhr.ocir.io --username apaccpt01/adwsw
 
 sudo docker pull lhr.ocir.io/apaccpt01/adwhol/adw-hol:1.0
 
-wget https://objectstorage.uk-london-1.oraclecloud.com/p/DEbSlnJAxz58LGz97bRQPwUsjo97Jmbufs2JEgbE0Vk/n/apaccpt01/b/adwterratorm/o/startParticipantContainers.sh
+wget https://objectstorage.uk-london-1.oraclecloud.com/p/DEbSlnJAxz58LGz97bRQPwUsjo97Jmbufs2JEgbE0Vk/n/apaccpt01/b/adwterratorm/o/startParticipantContainers.sh -P /home/opc/
 
-wget https://objectstorage.uk-london-1.oraclecloud.com/p/4copnNHswrNq8RZA-4FpHCo6rWpA7tU5gRfwzsP4UpE/n/apaccpt01/b/adwterratorm/o/stopAllContainers.sh
+wget https://objectstorage.uk-london-1.oraclecloud.com/p/4copnNHswrNq8RZA-4FpHCo6rWpA7tU5gRfwzsP4UpE/n/apaccpt01/b/adwterratorm/o/stopAllContainers.sh -P /home/opc/
 
 sudo chmod +x *.sh
 
